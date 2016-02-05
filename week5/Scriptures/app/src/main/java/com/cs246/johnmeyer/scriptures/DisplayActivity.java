e7e4fee6bd573fe77d852e99a48098695e5d2582
John Meyer <johnny@meyer.net>
2016-02-04 22:16:04 -0700

    Added the content for the scripture application. As of now, it is working. There is color, and a go back button that clears the screen

---
+++ week5/Scriptures/app/src/main/java/com/cs246/johnmeyer/scriptures/DisplayActivity.java
@@ -0,0 +1,38 @@
+package com.cs246.johnmeyer.scriptures;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView tv = (TextView)findViewById(R.id.textView4);
+        tv.setText(ScriptureActivity.passedText);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void onClickBack(View v) {
+        Intent i = new Intent(DisplayActivity.this, ScriptureActivity.class);
+        startActivity(i);
+    }
+}
