7ced6c21498ed5eafe33ef919ff8958b78dc2faa
John Meyer <johnny@meyer.net>
2016-02-10 12:23:24 -0700

    Added a event handler for the create button

--- week6/Multi-Threads/app/src/main/java/com/cs246/johnmeyer/multi_threads/MainActivity.java
+++ week6/Multi-Threads/app/src/main/java/com/cs246/johnmeyer/multi_threads/MainActivity.java
@@ -1,15 +1,24 @@
 package com.cs246.johnmeyer.multi_threads;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    FileOutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +57,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickWriteFile(View v) {
+        String fileName = "numbers.txt";
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+
+            try {
+                for (int i = 1; i < 11; i++) {
+                    writer.write(i + '\n');
+                    try {
+                        Thread.sleep(250);
+                    }
+                    catch (InterruptedException e) {
+                        // should never get here
+                    }
+                }
+                writer.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickLoadFile(View v) {
+
     }
 }
